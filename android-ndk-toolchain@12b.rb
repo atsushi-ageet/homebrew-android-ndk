714758b4df35bc8b82229c948f61b589445d09db
atsushi <atsushi@ageet.com>
2018-06-17 02:55:57 +0900

    Add make toolchain formula

---
+++ android-ndk-toolchain@12b.rb
@@ -0,0 +1,24 @@
+class AndroidNdkToolchainAT12b < Formula
+  desc "Android native-code language toolset"
+  homepage "https://developer.android.com/ndk/index.html"
+  url "https://gist.github.com/atsushi-ageet/738831f070e7134de843e46af3f572ed/archive/78227fc68eab3c1366604a45ae1e69059ed6b242.zip"
+  version "r12b"
+  sha256 "c8270673b416d4ebec625e5041b3ace07a3d4f88c4bb44919c3ba9b5c3d0ecd9"
+  version_scheme 1
+
+  bottle :unneeded
+
+  depends_on 'python' => :build
+  depends_on 'android-ndk@12b' => :build
+
+  def install
+    bin.mkpath
+    ENV['ANDROID_NDK_DIR'] = Formula['android-ndk@12b'].prefix
+    ENV['ANDROID_NDK_TOOLCHAIN'] = prefix
+    ENV['API'] = ENV['HOMEBREW_NDK_API'] || '14'
+    ENV['STL_VERSION'] = ENV['HOMEBREW_NDK_STL_VERSION'] || 'libc++'
+    ENV['ARCHS'] = ENV['HOMEBREW_NDK_ARCHS'] || 'arm arm64 x86 x86_64'
+    chmod 0755, "android-ndk-make-toolchain.sh"
+    system (buildpath/"android-ndk-make-toolchain.sh")
+  end
+end
